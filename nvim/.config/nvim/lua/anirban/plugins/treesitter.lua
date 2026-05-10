530f54a41d610730c0e15bb90792c9cd6291f078
Anirban Mondal <anyrban@gmail.com>
2026-05-10 17:28:43 +0100

    Treesitter migrated to the main branch

--- nvim/.config/nvim/lua/anirban/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/anirban/plugins/treesitter.lua
@@ -1,125 +1,110 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
 	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
 	dependencies = {
 		"windwp/nvim-ts-autotag",
-		"nvim-treesitter/nvim-treesitter-textobjects",
+		{
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			branch = "main",
+		},
 	},
 	config = function()
-		-- import nvim-treesitter configs
-		local treesitter = require("nvim-treesitter.configs")
+		local treesitter = require("nvim-treesitter")
 
-		-- configure treesitter
-		treesitter.setup({
-			-- enable syntax highlighting
-			highlight = { enable = true },
-			-- enable indentation
-			indent = { enable = true },
-			-- enable autotagging ( works with nvim-ts-autotagging plugin)
-			autotag = { enable = true },
-			-- make sure below language parsers are installed
-			ensure_installed = {
-				"awk",
-				"bash",
-				"bibtex",
-				"c",
-				"clojure",
-				"cmake",
-				"cpp",
-				"css",
-				"csv",
-				"cuda",
-				"dockerfile",
-				"gitignore",
-				"go",
-				"goctl",
-				"gpg",
-				"graphql",
-				"html",
-				"java",
-				"javascript",
-				"json",
-				"latex",
-				"llvm",
-				"lua",
-				"luadoc",
-				"make",
-				"markdown",
-				"markdown_inline",
-				"nginx",
-				"perl",
-				"php",
-				"phpdoc",
-				"printf",
-				"prisma",
-				"puppet",
-				"python",
-				"query",
-				"r",
-				"rust",
-				"ruby",
-				"scss",
-				"sparql",
-				"soql",
-				"sql",
-				"ssh_config",
-				"svelte",
-				"terraform",
-				"toml",
-				"tsx",
-				"typescript",
-				"vim",
-				"vimdoc",
-				"xml",
-				"yaml",
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
+		treesitter.install({
+			"awk",
+			"bash",
+			"bibtex",
+			"c",
+			"clojure",
+			"cmake",
+			"cpp",
+			"css",
+			"csv",
+			"cuda",
+			"dockerfile",
+			"gitignore",
+			"go",
+			"goctl",
+			"gpg",
+			"graphql",
+			"html",
+			"java",
+			"javascript",
+			"json",
+			"latex",
+			"llvm",
+			"lua",
+			"luadoc",
+			"make",
+			"markdown",
+			"markdown_inline",
+			"nginx",
+			"perl",
+			"php",
+			"phpdoc",
+			"printf",
+			"prisma",
+			"puppet",
+			"python",
+			"query",
+			"r",
+			"rust",
+			"ruby",
+			"scss",
+			"sparql",
+			"soql",
+			"sql",
+			"ssh_config",
+			"svelte",
+			"terraform",
+			"toml",
+			"tsx",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"xml",
+			"yaml",
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(args)
+				pcall(vim.treesitter.start, args.buf)
+
+				-- Tree-sitter indentation for all filetypes where a parser exists.
+				pcall(function()
+					vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end)
+			end,
+		})
+
+		require("nvim-treesitter-textobjects").setup({
+			select = {
+				lookahead = true,
+				selection_modes = {
+					["parameter.outer"] = "v",
+					["function.outer"] = "v",
+					["class.outer"] = "<c-v>",
 				},
-			},
-			textobjects = {
-				select = {
-					enable = true,
-					-- Automatically jump forward to textobj, similar to targets.vim
-					lookahead = true,
-					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
-						-- Anirban: We can find out more about below capture groups and its scheme file using :TSEditQuery command.
-						-- Eg. :TSEditQuery textobjects
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						-- You can optionally set descriptions to the mappings (used in the desc parameter of
-						-- nvim_buf_set_keymap) which plugins like which-key display
-						["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-						-- You can also use captures from other query groups like `locals.scm`
-						["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
-					},
-					-- You can choose the select mode (default is charwise 'v'). Other possible ones 'V' (for linewise) and '<c-v>'(blockwise)
-					-- mapping query_strings to modes.
-					selection_modes = {
-						["@parameter.outer"] = "v", -- charwise
-						["@function.outer"] = "v", -- linewise
-						["@class.outer"] = "<c-v>", -- blockwise
-					},
-					-- If you set this to `true` (default is `false`) then any textobject is
-					-- extended to include preceding or succeeding whitespace. Succeeding
-					-- whitespace has priority in order to act similarly to eg the built-in
-					-- `ap`.
-					--
-					-- Can also be a function which gets passed a table with the keys
-					-- * query_string: eg '@function.inner'
-					-- * selection_mode: eg 'v'
-					-- and should return true or false
-					include_surrounding_whitespace = true,
-				},
+				include_surrounding_whitespace = true,
 			},
 		})
+		vim.keymap.set({ "x", "o" }, "af", function()
+			require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+		end, { desc = "Select outer function" })
+
+		vim.keymap.set({ "x", "o" }, "if", function()
+			require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+		end, { desc = "Select inner function" })
+
+		vim.keymap.set({ "x", "o" }, "ac", function()
+			require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+		end, { desc = "Select outer class" })
+
+		vim.keymap.set({ "x", "o" }, "ic", function()
+			require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
+		end, { desc = "Select inner class" })
 	end,
 }
